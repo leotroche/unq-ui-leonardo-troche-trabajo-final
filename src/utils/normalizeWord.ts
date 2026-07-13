1c1af5276814a2a396f7d3264d262c7d3cc26653
Leonardo Troche <leo.m.troche@gmail.com>
2026-07-13 11:05:39 -0300

    feat: better normalizeWord function

--- src/utils/normalizeWord.ts
+++ src/utils/normalizeWord.ts
@@ -1,3 +1,7 @@
 export function normalizeWord(word: string): string {
-  return word.trim().toLowerCase()
+  return word
+    .trim()
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
 }
